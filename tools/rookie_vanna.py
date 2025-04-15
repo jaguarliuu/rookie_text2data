a0b00ebdf3c0e37114ad38822860b1f7662ac87a
jaguarliu <s23I602lP4ydBNRp9liTTC+jaguarliu@noreply.cnb.cool>
2025-04-15 18:30:15 +0800

    clean ak/sk

--- tools/rookie_vanna.py
+++ tools/rookie_vanna.py
@@ -8,9 +8,9 @@ from openai import OpenAI
 class RookieVannaTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
         client = OpenAI(
-            api_key='sk-pEdZ99blIAQr9JNL9d67100e9727446884256fA7D23c93Ae',
-            base_url='https://ai-yyds.com/v1',
-            model='gpt-4o'
+            api_key='s',
+            base_url='ht',
+            model=''
         )
         vanna = MyVanna(client=client)
         vanna.connect_to_mysql(host='106.54.240.161', dbname='rookie_im_server', user='root', password='8uhb^YJm', port=3306)
